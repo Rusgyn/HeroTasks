a02e58e76fc3fbf2cfb4729b71caba6a6f0e1ea6
rusgyn <rusgynmagsanay@gmail.com>
2025-04-24 16:49:07 -0400

    Create a test component and modify App file

---
+++ FrontEnd/src/components/Session.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Session = () => {
+
+  return (
+    <div>
+      <h1>Session</h1>
+      <p>This is the session component.</p>
+      <p>It will be used to manage user sessions.</p>
+    </div>
+  )
+}
+export default Session;
