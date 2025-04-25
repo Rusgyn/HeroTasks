99763a5f5913a90c1f726eaca0d31800928421a1
rusgyn <rusgynmagsanay@gmail.com>
2025-04-25 17:04:00 -0400

    Modify initial schema and seeds database

--- BackEnd/src/Database/initial.sql
+++ BackEnd/src/Database/initial.sql
@@ -2,8 +2,11 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
   email VARCHAR(100) UNIQUE NOT NULL,
+  password_digest VARCHAR(255) NOT NULL CHECK (LENGTH(password_digest) >= 7),
+  code CHAR(4) NOT NULL CHECK (code ~ '^\d{4}$'),
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -12,12 +15,13 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS tasks CASCADE;
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
+  hero_name VARCHAR(100),
   title VARCHAR(255),
   completed BOOLEAN DEFAULT FALSE,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- NOTE:
--- You can run this script using the following command: psql -U your_user -d your_database -f ./BackEnd/Database/init.sql
+-- You can run this script using the following command: psql -U your_user -d your_database -f your_file_path
---
+++ BackEnd/src/Database/seeds/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (first_name, last_name, email, password_digest, code)
+  VALUES('Jane', 'Smith', 'Jane.Smith@test.com', 'test1234', '0001');
+
+INSERT INTO users (first_name, last_name, email, password_digest, code)
+  VALUES('Bob', 'James', 'Bjames@test.com', '123qweASD', '3210');
