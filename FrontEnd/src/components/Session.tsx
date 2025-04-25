3d4ddbac868ee524d0dc46b6919bb0de73dedbc4
rusgyn <rusgynmagsanay@gmail.com>
2025-04-24 22:46:17 -0400

    Session mock up

--- FrontEnd/src/components/Session.tsx
+++ FrontEnd/src/components/Session.tsx
@@ -7,6 +7,19 @@ const Session = () => {
       <h1>Session</h1>
       <p>This is the session component.</p>
       <p>It will be used to manage user sessions.</p>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+
+      <p>Don't have an account? <a href="/register">Register here</a></p>
+      <p>Forgot your password? <a href="/forgot-password">Reset it here</a></p>
+      <p>Need help? <a href="/help">Contact us</a></p>
     </div>
   )
 }
