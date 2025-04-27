990fe7330df39e2e37cee1ccdadf9268bc3c09f5
rusgyn <rusgynmagsanay@gmail.com>
2025-04-27 15:42:05 -0400

    Update seeds according to the updated database

--- BackEnd/src/Database/seeds/tasks.sql
+++ BackEnd/src/Database/seeds/tasks.sql
@@ -1,25 +1,25 @@
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Wash Dishes', 1, 1);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Vacuum the house', 1, 1);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Scrub the floor', 1, 1);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Clean the garage', 2, 1);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Cook lunch', 2, 1);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Bake chocolate cake', 2, 1);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Cook dinner', 2, 1);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Fold laundry', 3, 1);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Wash Dishes', 1);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Vacuum the house', 1);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Scrub the floor', 1);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Clean the garage', 2);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Cook lunch', 2);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Bake chocolate cake', 2);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Cook dinner', 2);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Fold laundry', 3);
  
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Wipe the table', 1, 2);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Take out the thrash', 2, 2);
-INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Clean the pool', 2, 2);
-  INSERT INTO tasks (superpower, superhero_id, user_id)
-  VALUES('Organize the backyard tools', 2, 2);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Wipe the table', 1);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Take out the thrash', 2);
+INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Clean the pool', 2);
+  INSERT INTO tasks (superpower, superhero_id)
+  VALUES('Organize the backyard tools', 2);
