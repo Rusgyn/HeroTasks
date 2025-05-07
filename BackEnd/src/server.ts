a718af48457a5b6c3ca3b2f7eaa54f484cb3fe9b
rusgyn <rusgynmagsanay@gmail.com>
2025-05-07 17:16:42 -0400

    Modify the Pool file, and fix the connection to the proper server

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,24 +1,31 @@
-import express from "express";
-import morgan from "morgan"; // HTTP request logger
-import cors from "cors";
-import db from "./Database/db";
-import path from "path";
 import dotenv from "dotenv";
-import axios from "axios";
-import session from "express-session"; //maintain user state
-
-const app = express();
-const PORT = 3001;
-
-//handles dotenv for databasing. Load .env from a file.
+import path from "path";
 dotenv.config(
   {
     path: path.resolve(__dirname, '../.env')
   }
-);
+); //handles dotenv for databasing. Above format will ensure that db is loaded before anything and get the correct values.
 
-//Middleware
-//CORS middleware. Should be added before any other routes or middleware this ensures that the CORS headers are properly set in the response before any other logic 
+import express from "express";
+import morgan from "morgan"; // HTTP request logger
+import cors from "cors";
+import db from './Database/db';
+import axios from "axios";
+import session from "express-session"; //maintain user state
+/* DB Queries */
+import userQueries from "./Database/queries/usersQueries";
+import superheroQueries from "./Database/queries/superheroesQueries";
+import taskQueries from "./Database/queries/tasksQueries";
+/* Types */
+import User from "./types/userTypes";
+import { Superhero, NewSuperheroInput } from "./types/superheroTypes";
+import { Task, NewTaskInput } from "./types/taskTypes";
+
+const app = express();
+const PORT = 3001;
+
+/* Middleware */
+//CORS should be added before any other routes or middleware this ensures that the CORS headers are properly set in the response before any other logic 
 app.use(cors({
   origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -28,6 +35,15 @@ app.use(cors({
 app.use(morgan('dev')); // HTTP request logger
 app.use(express.json()); // Parse JSON payloads.
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded payloads
+
+/* DB Test - Development */
+db.query("SELECT * FROM users WHERE id = 1;")
+  .then((response) => console.log("DB Test, user table found: ", response.rows))
+  .catch((error) => console.error("DT Test, error querying users table:", error));
+  
+db.query('SELECT current_database();')
+  .then(res => console.log('✅ Connected to DB:', res.rows[0]))
+  .catch(err => console.error('❌ Error checking current DB:', err));
 
 /* User API routes */
 app.get('/', (_req, res) => {
