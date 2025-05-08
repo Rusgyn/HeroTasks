465b4fbb03e2db2d2817b6eda27e9eecd7dd8d6f
rusgyn <rusgynmagsanay@gmail.com>
2025-05-07 20:02:18 -0400

    Modify seeds to small letters for easy testing.

--- BackEnd/src/Database/seeds/tasks.sql
+++ BackEnd/src/Database/seeds/tasks.sql
@@ -11,15 +11,15 @@ INSERT INTO tasks (superpower, superhero_id)
 INSERT INTO tasks (superpower, superhero_id)
   VALUES('Bake chocolate cake', 2);
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Cook dinner', 2);
+  VALUES('Cook dinner', 3);
 INSERT INTO tasks (superpower, superhero_id)
   VALUES('Fold laundry', 3);
  
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Wipe the table', 1);
+  VALUES('Wipe the table', 4);
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Take out the thrash', 2);
+  VALUES('Take out the thrash', 4);
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Clean the pool', 2);
+  VALUES('Clean the pool', 4);
   INSERT INTO tasks (superpower, superhero_id)
   VALUES('Organize the backyard tools', 2);
--- BackEnd/src/Database/seeds/users.sql
+++ BackEnd/src/Database/seeds/users.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (first_name, last_name, email, password_digest, code)
-  VALUES('Jane', 'Smith', 'Jane.Smith@test.com', 'test1234', '0001');
+  VALUES('Jane', 'Smith', 'jane.smith@test.com', 'test1234', '0001');
 
 INSERT INTO users (first_name, last_name, email, password_digest, code)
-  VALUES('Bob', 'James', 'Bjames@test.com', '123qweASD', '3210');
+  VALUES('Bob', 'James', 'bjames@test.com', '123qweASD', '3210');
