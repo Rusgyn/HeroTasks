49c68aaccfb7c62bd059abf5408325139a0422e1
rusgyn <rusgynmagsanay@gmail.com>
2025-05-08 21:57:33 -0400

    Define Hero Dash board, adding a new backeng route to communicate to the database

--- FrontEnd/src/components/HeroTaskBoard.tsx
+++ FrontEnd/src/components/HeroTaskBoard.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Superhero } from '../types/Superhero';
 import '../styles/HeroTaskBoard.scss';
 
+
 const HeroTaskBoard = () => {
+
+  const [superheroes, setSuperheroes] = useState<Superhero[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchHeroesData = async() => {
+      try {
+        const response = await axios.get('/HeroTasks/superheroes-with-tasks');
+        console.log("Hero DashBoard. The Superheroes Data: ", response.data);
+        setSuperheroes(response.data);
+      } catch (error) {
+        console.error("Hero Dashboard. Error fetching data: ", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchHeroesData();
+  }, []);
+
+  if (loading) return <div className="loading"> Loading ... </div>;
+
   return (
     <div className="board_page">
 
