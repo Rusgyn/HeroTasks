9dc921bb2902d71880bb7e384cf1ab348c640cfc
rusgyn <rusgynmagsanay@gmail.com>
2025-06-27 14:58:08 -0400

    feat(ConfirmWithCode): A new compoent that will validate each request with code before execution, like when Deleting Superhero.

---
+++ FrontEnd/src/components/ConfirmWithCode.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const ConfirmWithCode = () => {
+
+  return (
+    <div>
+      <p> Confirm with Code!</p>
+    </div>
+  )
+
+}
+
+export default ConfirmWithCode;
