97efde9ab7a1509ab1720109487d55341a1a59ac
rusgyn <rusgynmagsanay@gmail.com>
2025-06-27 13:56:27 -0400

    db(queries): Modify the seeds content, the initial data.

--- BackEnd/src/Database/seeds/superheroes.sql
+++ BackEnd/src/Database/seeds/superheroes.sql
@@ -1,10 +1,10 @@
 INSERT INTO superheroes (superhero_name, user_id)
   VALUES('captain_obvious', 1);
 INSERT INTO superheroes (superhero_name, user_id)
-  VALUES('sgt_jane', 1);
+  VALUES('wolverine', 1);
 INSERT INTO superheroes (superhero_name, user_id)
-  VALUES('detective_benson', 1);
+  VALUES('hulk', 1);
 INSERT INTO superheroes (superhero_name, user_id)
   VALUES('spider_kid', 2);
 INSERT INTO superheroes (superhero_name, user_id)
-  VALUES('awesome_glen', 2);
+  VALUES('storm', 2);
--- BackEnd/src/Database/seeds/tasks.sql
+++ BackEnd/src/Database/seeds/tasks.sql
@@ -11,15 +11,15 @@ INSERT INTO tasks (superpower, superhero_id)
 INSERT INTO tasks (superpower, superhero_id)
   VALUES('Bake chocolate cake', 2);
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Cook dinner', 3);
+  VALUES('Cook dinner', 2);
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Fold laundry', 3);
+  VALUES('Fold laundry', 2);
  
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Wipe the table', 4);
+  VALUES('Wipe the table', 1);
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Take out the thrash', 4);
+  VALUES('Take out the thrash', 1);
 INSERT INTO tasks (superpower, superhero_id)
-  VALUES('Clean the pool', 4);
+  VALUES('Clean the pool', 2);
   INSERT INTO tasks (superpower, superhero_id)
   VALUES('Organize the backyard tools', 2);
