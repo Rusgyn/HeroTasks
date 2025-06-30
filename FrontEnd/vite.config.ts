a746fac7fb79ef22a362f724878128c19c2183bc
rusgyn <rusgynmagsanay@gmail.com>
2025-06-29 23:14:02 -0400

    Update vite configuration so dev and production behave consistently.

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         target: 'http://localhost:3001', // Backend URL. PORT: 3001
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/HeroTasks/, '')
+        //rewrite: (path) => path.replace(/^\/HeroTasks/, '')
       },
     }
   }
