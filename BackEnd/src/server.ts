c1b0dfcffe3d78791d0a667406c08abbf305e36f
rusgyn <rusgynmagsanay@gmail.com>
2025-07-02 17:35:04 -0400

    .

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -68,7 +68,7 @@ app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Or specify your domain
+  res.setHeader('Access-Control-Allow-Origin', 'https://hero-tasks.vercel.app/'); // Or specify your domain
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
